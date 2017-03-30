2ea92a42f200e99d61b2729915b4c81da021a069
Nicholas Engleman <nicholas@englemaninvest.com>
2017-03-30 00:32:46 -0400

    add footer
    added footer with increment functionality to keep track of items on list, filters to filter by active, completed or all, and a button to clear all completed items from the list

--- todo-app.js
+++ todo-app.js
@@ -2,6 +2,7 @@
 
 //Model
 var todoModel = {
+    showMode: 1,
     todos: [],
     //function to add todos
     addTodo: function(todo) {
@@ -12,21 +13,36 @@ var todoModel = {
     },
     toggleTodo: function(todo) {
         this.todos[todo].complete = !this.todos[todo].complete;
-    }
+    },
+
+    clearCompleteTodo: function() {
+         for(var i = 0; i < this.todos.length; i++) { 
+             if(this.todos[i].complete) { this.todos.splice(i, 1); }       
+             }
+        }
     
 }
 
 //View
 var todoShow = {
-
+    activeCount: 0,
+    completeExist: false,
     displayTodos: function() {
+        this.activeCount = 0;
+        this.completeExist = false;
         var parentContainer = document.querySelector("#parentContainer");
-
         while(parentContainer.firstChild) {
             parentContainer.removeChild(parentContainer.lastChild);
         }
-
+        //draw list of todos
         for(var i = 0; i < todoModel.todos.length; i++) {
+            if((todoModel.showMode == 1) ||
+              ((todoModel.showMode == 2) && (todoModel.todos[i].complete == true)) || 
+              ((todoModel.showMode == 3) && (todoModel.todos[i].complete == false))) {
+
+              if(todoModel.todos[i].complete == false) { this.activeCount++; }
+              if(todoModel.todos[i].complete) { this.completeExist = true; }
+
             //create todo container div
             var todoContainerDiv = document.createElement("div");
             todoContainerDiv.className = "todoContainer";
@@ -65,12 +81,59 @@ var todoShow = {
              todoContainerDiv.appendChild(deleteDiv);
              parentContainer.appendChild(todoContainerDiv);
 
+           }
+        }
+       this.createFooter();
+    },
+
+    createFooter: function() {
+        //create footer div
+        var footerDiv = document.createElement("div");
+        footerDiv.className = "footer";
+
+        var itemsLeftDiv = document.createElement("div");
+        itemsLeftDiv.className = "itemsLeftDiv";
+        itemsLeftDiv.innerHTML = "<p>"+this.activeCount+" items left</p>";
+
+        var actionDiv = document.createElement("div");
+        actionDiv.className = "actionDiv";
+        actionDiv.innerHTML = 
+             "<button id='all' onclick='handlers.viewAll(event)'>All</button>" +
+             "<button id='active' onclick='handlers.viewActive(event)'>Active</button>" +
+             "<button id='completed' onclick='handlers.viewCompleted(event)'>Completed</button>";
+
+        var clearCompletedDiv = document.createElement("div");
+        clearCompletedDiv.className = "clearCompletedDiv";
+        if(this.completeExist) { 
+            clearCompletedDiv.innerHTML = "<p onclick='handlers.clearCompleted()'>Clear Completed</p>";
+        }
+
+        footerDiv.appendChild(itemsLeftDiv);
+        footerDiv.appendChild(actionDiv);
+        footerDiv.appendChild(clearCompletedDiv);
+        parentContainer.appendChild(footerDiv);
+
+        this.addButtonBorder(); 
+    },
+
+    addButtonBorder: function() {
+        if(todoModel.showMode == 1) { 
+            var all = document.getElementById("all");
+            all.className = "selectedButtonBorder";
+        }
+        if(todoModel.showMode == 2) { 
+            var all = document.getElementById("completed");
+            all.className = "selectedButtonBorder";
+        }
+        if(todoModel.showMode == 3) { 
+            var all = document.getElementById("active");
+            all.className = "selectedButtonBorder";
         }
     }
 }
 
 //Controller
-var handlers = {
+var handlers = {    
     addTodo: function(e) {
         if(e.keyCode === 13) {
             var todotext = document.getElementById("addTodoTextInput");
@@ -88,7 +151,26 @@ var handlers = {
     toggleTodo: function(e) {
          todoModel.toggleTodo(e.currentTarget.parentNode.id);
          todoShow.displayTodos();
+    },
+   
+    viewAll: function(e) {
+        todoModel.showMode = 1;
+        todoShow.displayTodos();  
+    },
+
+    viewCompleted: function(e) {
+        todoModel.showMode = 2;
+        todoShow.displayTodos();  
+    },
+
+    viewActive: function(e) {
+        todoModel.showMode = 3; 
+        todoShow.displayTodos();   
+    },
+
+    clearCompleted: function() {
+        todoModel.clearCompleteTodo();
+        todoShow.displayTodos(); 
     }
-  
 }
 
